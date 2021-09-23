bde7921aefbd1f731ed7a81ac886ab0050d74cb8
iUser <author-main@mail.ru>
2021-09-23 12:06:30 +0300

    Initial commit

---
+++ app/src/main/java/com/education/appcomposeretrofit/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.education.appcomposeretrofit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.education.appcomposeretrofit.ui.theme.AppComposeRetrofitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppComposeRetrofitTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppComposeRetrofitTheme {
+        Greeting("Android")
+    }
+}
